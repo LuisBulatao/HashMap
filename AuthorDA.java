57931394a169db59e3db0c47102a0fd0df70ea87
LjBulatao <115807743+LuisBulatao@users.noreply.github.com>
2024-04-04 16:05:43 +0800

    Update AuthorDA.java

--- AuthorDA.java
+++ AuthorDA.java
@@ -1,15 +1,14 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class AuthorDA {
     private HashMap<String, Author> authorMap;
-    public  HashMap<String, Author> getAuthorMap() {
-        return authorMap;
-    }
-    public AuthorDA() throws FileNotFoundException{
-        Scanner AuthorInfo = new Scanner(new FileReader("Author.csv"));
+    
+    public AuthorDA() throws FileNotFoundException {
+        Scanner AuthorInfo = new Scanner(new FileReader("C:\\Users\\Luis Joshua\\OOPActivities\\LabSeatwork5\\src\\Author.csv"));
 
         authorMap = new HashMap<>();
 
@@ -18,14 +17,14 @@ public class AuthorDA {
             String[] ArrayAuthorInfo = new String[2];
             ArrayAuthorInfo = authorInfo.split(",");
 
-            Author author1 = new Author();
 
-            author1.setBio(ArrayAuthorInfo[0].trim());
-            author1.setName(ArrayAuthorInfo[1].trim());
+            String name = ArrayAuthorInfo[0].trim();
+            String bio = ArrayAuthorInfo[1].trim();
+            authorMap.put(name, new Author(name, bio));
 
-
-            authorMap.put(author1.getBio(), author1);
         }
-        AuthorInfo.close();
     }
+    public HashMap<String, Author> getAuthorMap() {
+        return authorMap;
+   }
 }
