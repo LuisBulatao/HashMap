28bec2ab3cc5db56ba3972f36099d1045a6e4505
LjBulatao <115807743+LuisBulatao@users.noreply.github.com>
2024-04-04 16:07:11 +0800

    Update BookReport.java

--- BookReport.java
+++ BookReport.java
@@ -1,22 +1,21 @@
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.*;
-
 public class BookReport {
-    public static void main(String args[]) throws FileNotFoundException {
-        BookDA book = new BookDA();
-        AuthorDA author = new AuthorDA();
+    public static void main(String args[]) throws IOException {
 
+        AuthorDA author1 = new AuthorDA();
+        HashMap<String, Author> authorMap = author1.getAuthorMap();
 
-        HashMap<String, Book> bookMap = book.getBookMap();
-        HashMap<String, Author> authorMap = author.getAuthorMap();
+        BookDA bookDA = new BookDA(authorMap);
+        HashMap<String, Book> bookMap = bookDA.book1();
 
-        for(Map.Entry<String, Book> Entry: bookMap.entrySet()) {
-            Book book1 = Entry.getValue();
-            Author author1 = book.;
+        for (Map.Entry<String, Book> entry : bookMap.entrySet()) {
+            Book book = entry.getValue();
+            Author author = book.getAuthor();
 
-
-
-
+            System.out.println(book.getIsbn() + " " + book.getTitle());
+            System.out.println("\t" + author.getName() + " - " + author.getBio() + "\n");
         }
     }
+
 }
